f497e6def57dd584c68a81fc1f9de9e40e66b888
faiisll <faisal.ayas72@gmail.com>
2020-04-26 19:04:36 +0700

    Updates

---
+++ precache-manifest.1c6f61430ead49ad16f9be10ad794a7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "823c9ecfbf116b6c40cb340120e457cd",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "7c7a1e4e078bac87598b",
+    "url": "/recipe-app/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "3d5be29aa48ff67af17c",
+    "url": "/recipe-app/static/css/main.e7a95469.chunk.css"
+  },
+  {
+    "revision": "7c7a1e4e078bac87598b",
+    "url": "/recipe-app/static/js/2.d4304365.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-app/static/js/2.d4304365.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d5be29aa48ff67af17c",
+    "url": "/recipe-app/static/js/main.dc87aa7e.chunk.js"
+  },
+  {
+    "revision": "6c108a12729393597590",
+    "url": "/recipe-app/static/js/runtime-main.2b464d9d.js"
+  }
+]);
